b15972bf6608a020e4af3bfe1d81b7c427c49c1f
Nastaran <nastaran.eskandari@gmail.com>
2019-07-12 11:59:00 -0400

    update form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const Form = (props) =>(
-    <form className="form" onSubmit={props.getRecipe} style={{margin:"2rem"}}>
+    <form className="form" onSubmit={} style={{margin:"2rem"}}>
         <input className="form__input" type="text" name="recipeName" />
         <button className="form__button">Search</button>
     </form>
